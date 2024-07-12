697db0804763e8fdb1dd73223d88a2ad3838c50f
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-07-12 10:47:12 -0500

    gen supabase db types

--- src/actions/supabase.ts
+++ src/actions/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const url = process.env.NEXT_PUBLIC_DB_URL;
+const key = process.env.NEXT_PUBLIC_DB_ANON_KEY;
 export const client = () => {
   if (url && key) {
     return createClient(url, key);
